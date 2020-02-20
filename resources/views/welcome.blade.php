ca7939e1929c73a0413c31ac58f5a77baab5f644
Alwin Crasta <alwyncrasta000@gmail.com>
2020-02-20 16:01:03 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<main id="main-container" style="height: 80vh;"> 
+    <div class="bg-image" style="background-image: url('assets/media/photos/photo2@2x.jpg');">
+        <div class="hero bg-primary-dark-op">
+            <div class="hero-inner">
+                <div class="content content-full text-center">
+                    <h1 class="display-4 font-w700 text-white mb-10 js-appear-enabled animated fadeInDown" data-toggle="appear" data-class="animated fadeInDown">Learn On Go!</h1>
+                    <h2 class="font-w400 text-white-op mb-50 js-appear-enabled animated fadeInUp" data-toggle="appear" data-class="animated fadeInUp" data-timeout="250">Lorem ipsum dolor sit amet.</h2>
+                        
+                </div>
+            </div>
+        </div>
+    </div>  
+</main>
+@endsection
